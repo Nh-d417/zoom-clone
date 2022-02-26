5ce0323353109683c16e5a176079f21830793c1c
Nh-d417 <alfktngu33@gmail.com>
2022-02-26 23:06:29 +0900

    webSocket_Event

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,58 @@
-alert("hohoho");
+const messageList = document.querySelector("ul");
+const nickForm =document.querySelector("#nick");
+const messageForm =document.querySelector("#message");
+//socket : 서버로의 연결
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+
+//닉네임과 메시지를 구별하기 위해 오브젝트 형성
+// 백엔드에 문자타입으로 전달해야 하므로 stringify사용
+function makeMessage(type, payload){
+  const msg = {type, payload};
+  return JSON.stringify(msg);
+}
+
+
+socket.addEventListener("open", ()=>{
+  console.log("Connected to Server ✔");
+});
+
+socket.addEventListener("message", (message)=>{
+  const li = document.createElement("li");
+  li.innerText = message.data;
+
+  messageList.append(li);
+
+});
+
+socket.addEventListener("close", ()=>{
+  console.log("Connected to Server ❌");
+});
+
+// setTimeout(() => {
+//   socket.send("hello from the browser ");
+// }, 10000);
+
+
+
+
+function handleSubmit(event){
+  event.preventDefault();
+  const input = messageForm.querySelector("input");
+
+  //백엔드로 메시지 송신후 , 인풋을 비워주기(초기화)
+  socket.send(makeMessage("new_message", input.value));
+  input.value = "";
+
+}
+
+function handleNickSubmit(event){
+  event.preventDefault();
+  const input = nickForm.querySelector("input");
+  socket.send(makeMessage("nickname", input.value));
+  input.value = "";
+}
+
+
+messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit)
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -17,4 +19,45 @@ app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
-app.listen(3000, handleListen);
+//app에 등록된 http서버에 acess
+const server = http.createServer(app);
+//http서버 위로 Websocket서버를 생성
+// http와 ws서버 둘다 처리가 가능하게 됨
+const wss = new WebSocket.Server({server});
+
+
+//브라우저대응을 위한 가짜 데이터베이스 용
+const sockets = [];
+
+wss.on("connection",  (socket) =>{
+  sockets.push(socket);
+  socket["nickname"] = "Anon";
+   //socket : 연결된 브라우저
+  console.log("Connected to Browser");
+
+  //브라우저 창이 닫힐 때 터미널 로그출력
+  socket.on("close", () =>console.log("Connected to Browser ❌"));
+
+  //브라우저로부터 메시지 받기
+  socket.on("message", (msg)=>{
+    const message = JSON.parse(msg.toString('utf8'));
+    
+    switch(message.type){
+      case "new_message":
+        //브라우저에서 받은 메시지를 다시 보내기
+        // socket.send(message.toString('utf8'));
+        sockets.forEach((aSocket) => aSocket.send(
+          `${socket.nickname}: ${message.payload}`));
+      
+      case "nickname":
+        socket["nickname"] = message.payload;
+    }
+    
+     
+    //브라우저에 메시지 보내기
+    // socket.send(msg.toString('utf8'));
+  });
+
+});
+
+server.listen(3000, handleListen);
